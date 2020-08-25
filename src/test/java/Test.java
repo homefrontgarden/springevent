4605fe82d4e761964e01ca1fc9da46fd142a374e
homefrontgarden <dengjingsi666@163.com>
2020-08-25 22:06:41 +0800

    spring事件添加web依赖

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,11 +1,15 @@
 import com.djs.spring.EmailBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
 
 public class Test {
     public static void main(String[] args) {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-event.xml");
-        EmailBean email = (EmailBean) ctx.getBean("emailer");
+//        EmailBean email = (EmailBean) ctx.getBean("emailer");
+        EmailBean email = ctx.getBean(EmailBean.class);
+//        WebApplicationContext context =
+        //学习ApplicationContext与WebApplicationContext获取，应用，之间的关系
         email.sendEmail("black@list.org");
     }
 }
